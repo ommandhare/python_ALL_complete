751422545f381a7b9d2c026488ddc9ab1aa26d5f
Om Mandhare <ommandhare007@gmail.com>
2024-08-02 17:21:17 +0530

    new changes

---
+++ analysis_project/OBJECTIVE QUERIES/NEW ANALYSIS/affinityScore.sql
@@ -0,0 +1,17 @@
+-- combination with ratio (affinity score)
+ use retail_project;
+ select target1, target2,concat(target1,"_",target2) as combination_pair,combo_count, count as target1_count,(combo_count/count) as affinity_score
+ from
+ (
+select d1.product_id as target1 ,d2.product_id as target2,count(d1.tran_id) as combo_count from tran_dtl d1
+join tran_dtl d2 on d1.tran_id=d2.tran_id
+where d1.product_id!=d2.product_id  -- and d1.product_id='100'
+group by d1.product_id,d2.product_id 
+order by combo_count desc
+)a
+join (
+select product_id,count(product_id) as count from tran_dtl
+group by product_id)b
+on a.target1=b.product_id
+;
+ -- WHERE td.tran_dt < current_date() AND td.tran_dt >= DATE_SUB(CURRENT_DATE(), interval 1 year)
---
+++ analysis_project/new.md
@@ -0,0 +1,4 @@
+## hi hello
+my name is om
+
+* hello

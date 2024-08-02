a2792434f576b00353b1036f0b8e3831c47256f1
Om Mandhare <ommandhare007@gmail.com>
2024-08-02 15:07:26 +0530

    new change

--- newtest.py
+++ newtest.py
@@ -5,7 +5,7 @@ from sqlalchemy import create_engine
 
 
 
-path= r"C:\Users\Om Mandhare\Documents\moving_avg_fact.csv" #path of csv file
+path= r"C:\Users\Om Mandhare\Documents\rsi.csv" #path of csv file
 
 data=pd.read_csv(path)
 
@@ -13,6 +13,6 @@ print(data)
 
 engine = create_engine('mysql+pymysql://root:0777@localhost:3306/stock')  #username pass database name
 
-data.to_sql('moving_avg_fact', con=engine, if_exists='replace', index=False)
+data.to_sql('rsi', con=engine, if_exists='replace', index=False)
 # table name
 print("done")
